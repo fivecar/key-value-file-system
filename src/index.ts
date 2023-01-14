62b5fd6efc8f8fb97693a1f27e2cf554b2a043c7
Philip Su <39933441+fivecar@users.noreply.github.com>
2023-01-14 00:18:41 -0800

    feat: fully working and tested implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,155 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+export interface KeyValueStore {
+  getAllKeys(): Promise<readonly string[]>;
+  getItem(key: string): Promise<string | null>;
+  setItem(key: string, value: string): Promise<void>;
+  removeItem(key: string): Promise<void>;
+  multiGet(
+    keys: readonly string[]
+  ): Promise<readonly [string, string | null][]>;
+  multiSet(keyValuePairs: Array<[string, string]>): Promise<void>;
+  multiRemove(keys: readonly string[]): Promise<void>;
+}
+
+export interface PathValue<T> {
+  path: string;
+  value: T;
+}
+
+export default class KeyValueFileSystem {
+  store: KeyValueStore;
+  prefix: string;
+
+  constructor(store: KeyValueStore, keyNamespacePrefix = "/kvfs") {
+    if (!keyNamespacePrefix.length) {
+      throw new Error(
+        "keyNamespacePrefix must not be empty. This protects you from\
+        accidentally deleting everything else in your store."
+      );
+    }
+    this.store = store;
+    this.prefix = keyNamespacePrefix;
+  }
+
+  async ls(spec?: string): Promise<readonly string[]> {
+    const normalizedSpec = spec || "*";
+    const keys = await this.store.getAllKeys();
+    const regex = this.regexFromSpec(normalizedSpec);
+
+    return keys
+      .filter(key => regex.test(key))
+      .map(key => key.slice(this.prefix.length));
+  }
+
+  async read<T>(path: string): Promise<T | null> {
+    this.validatePath(path);
+    const item = await this.store.getItem(this.prefix + path);
+
+    if (item !== null) {
+      return JSON.parse(item) as T;
+    }
+    return item;
+  }
+
+  async readMulti<T>(spec: string): Promise<readonly PathValue<T | null>[]> {
+    const keys = await this.ls(spec);
+    const values = await this.store.multiGet(
+      keys.map(key => this.prefix + key)
+    );
+
+    return values.map(([path, str]) => {
+      const value = str ? (JSON.parse(str) as T) : null;
+      return {
+        path: path.slice(this.prefix.length),
+        value,
+      };
+    });
+  }
+
+  async write<T>(path: string, value: T): Promise<void> {
+    this.validatePath(path);
+    return await this.store.setItem(this.prefix + path, JSON.stringify(value));
+  }
+
+  /**
+   * @param basePath The base path to prepend to each subPath in values.
+   * @param values Each subPath and object you want written
+   */
+  async writeMulti<T>(
+    basePath: string | undefined,
+    values: PathValue<T>[]
+  ): Promise<void> {
+    if (values.length === 0) {
+      return;
+    }
+
+    const keyValues = values.map(({ path: subPath, value }): [
+      string,
+      string
+    ] => {
+      const fullPath = basePath ? `${basePath}${subPath}` : subPath;
+
+      this.validatePath(fullPath);
+      return [this.prefix + fullPath, JSON.stringify(value)];
+    });
+
+    return await this.store.multiSet(keyValues);
+  }
+
+  async rm(path: string): Promise<void> {
+    this.validatePath(path);
+    return await this.store.removeItem(this.prefix + path);
+  }
+
+  async rmAllForce(): Promise<void> {
+    const keys = await this.store.getAllKeys();
+    const ourKeys = keys.filter(key => key.startsWith(this.prefix));
+
+    return await this.store.multiRemove(ourKeys);
+  }
+
+  private validatePath(path: string): void {
+    if (path.length === 0) {
+      throw new Error("spec or path must not be empty");
+    }
+  }
+
+  private regexFromSpec(spec: string): RegExp {
+    const segments = [];
+    let i = 0;
+    let curSegment = "";
+
+    while (i < spec.length) {
+      const char = spec[i];
+
+      switch (char) {
+        case "\\":
+          i++;
+          curSegment += spec[i];
+          break;
+        case "*":
+          if (curSegment.length) {
+            segments.push(curSegment);
+            curSegment = "";
+          }
+          break;
+        default:
+          curSegment += char;
+          break;
+      }
+      i++;
+    }
+    if (curSegment.length) {
+      segments.push(curSegment);
+    }
+
+    // Escapes all regex special characters, from ES7 proposal
+    // https://stackoverflow.com/a/63838890
+    const segRegex = segments
+      .map(seg => seg.replace(/[.*+\-?^${}()|[\]\\]/g, "\\$&"))
+      .join(".*");
+
+    return new RegExp(
+      `^${this.prefix}${segRegex}${spec[spec.length - 1] === "*" ? ".*" : ""}$`
+    );
+  }
+}
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,182 @@
-import { myPackage } from '../src';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import KeyValueFileSystem from "../src";
 
-describe('index', () => {
-  describe('myPackage', () => {
-    it('should return a string containing the message', () => {
-      const message = 'Hello';
+const store = new KeyValueFileSystem(AsyncStorage);
+const file = { hello: "world" };
 
-      const result = myPackage(message);
+describe("KeyValueStore", () => {
+  beforeEach(async () => {
+    await AsyncStorage.clear();
+  });
 
-      expect(result).toMatch(message);
-    });
+  it("should require a prefix", () => {
+    expect(() => new KeyValueFileSystem(AsyncStorage, "")).toThrow();
+  });
+
+  it("should refuse empty paths", async () => {
+    await expect(store.ls("")).rejects.toThrow();
+    await expect(store.read("")).rejects.toThrow();
+    await expect(store.write("", file)).rejects.toThrow();
+    await expect(store.rm("")).rejects.toThrow();
+    await expect(
+      store.writeMulti("", [{ path: "", value: file }])
+    ).rejects.toThrow();
+  });
+
+  it("should start as empty", async () => {
+    const files = await store.ls("*");
+
+    expect(files).toHaveLength(0);
+  });
+
+  it("should write one object", async () => {
+    await store.write("/home", file);
+
+    const written = await store.read("/home");
+    expect(written).toEqual(file);
+  });
+
+  it("should always use prefixes", async () => {
+    const tempStore = new KeyValueFileSystem(AsyncStorage, "/test_prefix");
+    const otherKeys = ["foo", "bar", "baz"];
+
+    await Promise.all([
+      ...otherKeys.map(key => AsyncStorage.setItem(key, "secretstuff")),
+      tempStore.write("/home", file),
+      tempStore.write("/page", file),
+    ]);
+    const [paths, allKeys] = await Promise.all([
+      tempStore.ls("/"),
+      AsyncStorage.getAllKeys(),
+    ]);
+
+    expect(paths).toHaveLength(2);
+    expect(allKeys).toHaveLength(5);
+    expect(allKeys).toEqual(
+      expect.arrayContaining([
+        ...otherKeys,
+        "/test_prefix/home",
+        "/test_prefix/page",
+      ])
+    );
+  });
+
+  it("should write nothing when given nothing in writeMulti", async () => {
+    await store.writeMulti("/base", []);
+
+    const files = await store.ls("/base");
+    expect(files).toHaveLength(0);
+  });
+
+  it("should remove all its keys when forced", async () => {
+    const otherKeys = ["foo", "bar", "baz"];
+
+    await Promise.all([
+      ...otherKeys.map(key => AsyncStorage.setItem(key, "secretstuff")),
+      store.write("/home", file),
+      store.write("/page", file),
+    ]);
+    await store.rmAllForce();
+    const [paths, allKeys] = await Promise.all([
+      store.ls("/"),
+      AsyncStorage.getAllKeys(),
+    ]);
+
+    expect(paths).toHaveLength(0);
+    expect(allKeys).toHaveLength(otherKeys.length);
+    expect(allKeys).toEqual(expect.arrayContaining(otherKeys));
+  });
+
+  it("should remove a single file", async () => {
+    await store.writeMulti("/base", [
+      { path: "/home", value: file },
+      { path: "/page", value: file },
+    ]);
+    await store.rm("/base/home");
+
+    const files = await store.readMulti<typeof file>("/base");
+    expect(files).toHaveLength(1);
+    expect(files[0].path).toEqual("/base/page");
+    expect(files[0].value).toEqual(file);
+  });
+
+  it("should return null for missing files", async () => {
+    const missing = await store.read("/home");
+
+    expect(missing).toBeNull();
+  });
+
+  it("should handle basic wildcard", async () => {
+    const otherKeys = ["foo", "bar", "baz"];
+
+    await Promise.all([
+      ...otherKeys.map(key => AsyncStorage.setItem(key, "secretstuff")),
+      store.write("/home", file),
+      store.write("/page", file),
+    ]);
+    const [paths, samePaths, moreSamePaths, allKeys] = await Promise.all([
+      store.ls(),
+      store.ls("*"),
+      store.ls("/*"),
+      AsyncStorage.getAllKeys(),
+    ]);
+    expect(paths.length).toBe(2);
+    expect(allKeys).toHaveLength(5);
+    expect(allKeys).toEqual(
+      expect.arrayContaining([...otherKeys, "/kvfs/home", "/kvfs/page"])
+    );
+    expect(paths).toEqual(samePaths);
+    expect(paths).toEqual(moreSamePaths);
+  });
+
+  it("should handle tail wildcards", async () => {
+    const otherKeys = ["foo", "bar", "baz"];
+
+    await Promise.all([
+      ...otherKeys.map(key => AsyncStorage.setItem(key, "secretstuff")),
+      store.write("/foo12", file),
+      store.write("/foo24", file),
+      store.write("/fom", file),
+      store.write("/bar", file),
+    ]);
+    const paths = await store.ls("/foo*");
+
+    expect(paths).toEqual(expect.arrayContaining(["/foo12", "/foo24"]));
+  });
+
+  it("should handle mid wildcards", async () => {
+    const otherKeys = ["foo", "bar", "baz"];
+
+    await Promise.all([
+      ...otherKeys.map(key => AsyncStorage.setItem(key, "secretstuff")),
+      store.write("/baseball", file),
+      store.write("/basketball", file),
+      store.write("/ball", file),
+      store.write("/baller", file),
+      store.write("/basketweaving", file),
+    ]);
+    const paths = await store.ls("/*ball");
+
+    expect(paths).toEqual(
+      expect.arrayContaining(["/baseball", "/basketball", "/ball"])
+    );
+  });
+
+  it("should handle mid-and-tail wildcards", async () => {
+    const otherKeys = ["foo", "bar", "baz"];
+
+    await Promise.all([
+      ...otherKeys.map(key => AsyncStorage.setItem(key, "secretstuff")),
+      store.write("/baseball", file),
+      store.write("/basketball", file),
+      store.write("/ball", file),
+      store.write("/baller", file),
+      store.write("/basketweaving", file),
+    ]);
+    const paths = await store.ls("/*ball*");
+
+    expect(paths).toEqual(
+      expect.arrayContaining(["/baseball", "/basketball", "/ball", "/baller"])
+    );
   });
 });
